426471c76582e66105f8ec2294771f46c94dadb6
jeromedusanter95 <dusanterjerome@gmail.com>
2019-09-30 15:17:08 +0200

    Add component BookList

---
+++ components/BookList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { StyleSheet, FlatList } from 'react-native'
+import BookItem from './BookItem'
+
+class BookList extends React.Component {
+  
+  render(){
+    return(
+      <FlatList
+        style={styles.list}
+        data={this.props.books}
+        keyExtractor={(item) => item.book_uri.toString()}
+        renderItem={({item}) => (
+            <BookItem  book= {item}/>
+          )}
+        />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  list: {
+    flex: 1
+  }
+})
+
+export default BookList
