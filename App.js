5dbd77c843ad6324a037eb8e7d2fb46d92effe0b
jeromedusanter95 <dusanterjerome@gmail.com>
2019-09-30 21:09:18 +0200

    Create store for all project

--- App.js
+++ App.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import BestSellerList from './components/BestSellerList'
 import Navigation from './navigation/Navigation'
+import { Provider } from 'react-redux'
+import Store from './store/configureStore'
 
 export default function App() {
   return (
-      <Navigation/>
+    <Provider store= {Store}>
+        <Navigation/>
+      </Provider>
   );
 }
